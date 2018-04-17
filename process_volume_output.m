b88c91d88d3b4e226e1c2e0a0fbccff0aa147b8e
jgaines42 <37844885+jgaines42@users.noreply.github.com>
2018-04-17 15:43:58 -0400

    Add files via upload

---
+++ process_volume_output.m
@@ -0,0 +1,24 @@
+function []=  process_volume_output(PDB, folder1, num_atoms)
+    edge_data = zeros(num_atoms, 100);
+    vol_data = zeros(num_atoms, 100);
+
+    for j = 1:100
+        file_name = strcat(folder_name,PDB, num2str(j), '.txt');
+        inputFile = fopen(file_name);
+        volumes_data  = textscan(inputFile, '%f %f %f');
+        edge_data(:,j) =volumes_data{2};
+        vol_data(:,j) = volumes_data{3};
+
+        fclose(inputFile);
+       
+    end
+    edge_data = max(edge_data,[],2);
+    vol_data = mean(vol_data,2);
+    
+    f = fopen(strcat(folder1, PDB, '_vol.txt'));
+    for i = 1:num_atoms
+            printf(f, '%d %f \n', edge_data(i), vol_data(i));
+    end
+    fclose(f);
+    
+end
